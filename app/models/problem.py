79727d3e784e2f17527dc354068d873d9f853edc
Tuhin Mandal <73009536+MandalTuhin@users.noreply.github.com>
2025-12-18 15:42:18 +0530

    feat(api): add validation logic for the problem model

--- app/models/problem.py
+++ app/models/problem.py
@@ -1,14 +1,20 @@
-from pydantic import BaseModel
 from enum import Enum
+
+from pydantic import BaseModel, Field
+
 
 class Diffiulty(str, Enum):
     EASY = "Easy"
     MEDIUM = "Medium"
-    DIFFICULT = "Difficult" # I am adding "difficult" for now, I may change it later.
+    DIFFICULT = "Difficult"  # I am adding "difficult" for now, I may change it later.
+
 
 class Problem(BaseModel):
-    id: int
-    title: str
-    difficulty: Diffiulty # Now restricted to the Enum values
+    # ge = Greater than or equal tos
+    id: int = Field(ge=1, description="The LeetCode problem Number")
+    # min_length = Minimum string length
+    title: str = Field(min_length=1, max_length=100)
+
+    difficulty: Diffiulty  # Now restricted to the Enum values
     tags: list[str] = []
-    solution_cpp: str | None = None
+    solution_cpp: str | None = Field(None, description="The C++ solution Code")
